90f81f7d33f5ca47915c6aff7159b59f702f0c8d
sergiomansoo <sergio.manso2007@gmail.com>
2026-02-02 11:07:29 -0300

    feat: abstracao do problema

---
+++ 02-poo/banco-digital/src/ContaCorrente.java
@@ -0,0 +1,16 @@
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+}
